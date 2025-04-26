8d0fca307f6cce684cde8767c454202074d1410b
TSUKAPOM-ktcm <tsukasa.m.311@gmail.com>
2025-04-26 10:02:26 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1281,4 +1281,4 @@ window.openNurseryEditModalByDate = openNurseryEditModalByDate;
 window.showOkaimonoModal = showOkaimonoModal;
 window.showOkaimonoEditModal = showOkaimonoEditModal;
 window.showDoneTasksModal = showDoneTasksModal;
-}
+
